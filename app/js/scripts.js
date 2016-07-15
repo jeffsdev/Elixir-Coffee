ccafd9e017843770f3cbdaee093e3b0d66c32574
Jeff Seymour <seym1585@gmail.com>
2016-07-15 15:43:47 -0700

    adjust styling on navbar and improve functionality

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -22,22 +22,37 @@ $(document).ready(function() {
   // toggle mobile nav menu
 
   $(".nav-burger").click(function() {
-    $('ul').toggleClass("active");
+
+    // remove scroll stop on body
+    $("body").unbind("touchmove");
+
+    // check to see if mobile-menu ul is already active
+    if ( $(".mobile-menu ul").hasClass("active") ) {
+      null;
+    } else {
+      $('.mobile-menu ul').toggleClass("active");
+    }
+
     $(".mobile-menu").toggleClass("active");
+    $(".navshroud").fadeToggle("slow");
+
+    // prevent user from scrolling on page when mobile menu is active
+    if ( $(".mobile-menu").hasClass("active") ) {
+      $("body").bind("touchmove",function(e){
+          e.preventDefault();
+      });
+    }
+
+      $('.nav-burger span:nth-child(2)').toggleClass('transparent');
+      $('.nav-burger span:nth-child(1)').toggleClass('rotate-top');
+      $('.nav-burger span:nth-child(3)').toggleClass('rotate-bottom');
+
   });
+
 
 
   /************************************************************/
   // toggle navbar visibility based on scroll position on page
-
-
-  // // make sure navbar does not hide if mobile menu is active
-  // if ($(".mobile-menu").hasClass("active")) {
-  //   $(window).on('scroll', function(){
-  //       nav.removeClass("hidden");
-  //       return false;
-  //   });
-
 
   // Hide Header on on scroll down
   var didScroll;
@@ -56,40 +71,26 @@ $(document).ready(function() {
       }
   }, 250);
 
-
-
   function hasScrolled() {
-      var st = $(this).scrollTop();
+    var st = $(this).scrollTop();
 
-      // Make sure they scroll more than delta
-      if(Math.abs(lastScrollTop - st) <= delta)
-          return;
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
 
-      // If they scrolled down and are past the navbar, add class .nav-up.
-      // This is necessary so you never see what is "behind" the navbar.
-      // make sure navbar does not hide if mobile menu is active
-      if ($(".mobile-menu").hasClass("active")) {
-        $(window).on('scroll', function(){
-            $(".main-navbar").removeClass("hidden");
-            
-        });
-      } else if (st > lastScrollTop && st > navbarHeight){
-          // Scroll Down
-          $('.main-navbar').removeClass('not-hidden').addClass('hidden');
-      } else {
-          // Scroll Up
-          if(st + $(window).height() < $(document).height()) {
-              $('.main-navbar').removeClass('hidden').addClass('not-hidden');
-          }
-      }
+    // Hide navbar when scrolling down, and show when scrolling up
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.main-navbar').removeClass('not-hidden').addClass('hidden');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.main-navbar').removeClass('hidden').addClass('not-hidden');
+        }
+    }
 
-
-
-      lastScrollTop = st;
+    lastScrollTop = st;
   }
-
-
-
 
 
   /************************************************************/
@@ -101,9 +102,29 @@ $(document).ready(function() {
     var id = $el.attr('href');
     var nav_height = $("nav").outerHeight();
 
+    // remove scroll stop on body
+    $("body").unbind("touchmove");
+
+    // determine when nav burger should animate back to normal
+    if ( (id === "#elixir-coffee") && $(".mobile-menu").hasClass("active") ) {
+      $('.nav-burger span:nth-child(2)').toggleClass('transparent');
+      $('.nav-burger span:nth-child(1)').toggleClass('rotate-top');
+      $('.nav-burger span:nth-child(3)').toggleClass('rotate-bottom');
+    } else if ( (id === "#elixir-coffee") && !$(".mobile-menu").hasClass("active")) {
+      null;
+    } else {
+      $('.nav-burger span:nth-child(2)').toggleClass('transparent');
+      $('.nav-burger span:nth-child(1)').toggleClass('rotate-top');
+      $('.nav-burger span:nth-child(3)').toggleClass('rotate-bottom');
+    }
+
     // hide mobile menu after clicking a link
-    $('ul').toggleClass("active");
-    $(".mobile-menu").toggleClass("active");
+    $('.mobile-menu ul').toggleClass("active");
+    $(".mobile-menu").removeClass("active");
+
+    if ( !$(".mobile-menu").hasClass("active") ) {
+      $(".navshroud").fadeOut("slow");
+    }
 
     // scroll to location
     $('html, body').animate({
@@ -127,25 +148,6 @@ $(document).ready(function() {
       opacity: 0
     }, 600);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
